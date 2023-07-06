4274f85f83daaed1e7d8f296e32d5e6e4473faa5
Ubuntu <ubuntu@ip-172-31-20-58.us-east-2.compute.internal>
2023-07-06 14:17:32 +0000

    Initial Commits

---
+++ article_api/spec/models/article_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  it "tests article object" do
+      article = FactoryBot.create(:article)
+      expect(article.title).to eq('Sample article')
+  end
+end
